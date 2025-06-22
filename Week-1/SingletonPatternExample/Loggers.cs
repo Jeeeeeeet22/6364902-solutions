5dc369e3b35549b29053c9efcc39185d0a6d8d61
Jeeeeeeet22 <jeetsahoo2203@gmail.com>
2025-06-22 12:51:34 +0530

    C:\Users\KIIT\6364902-solutions\Week-1\SingletonPatternExample

---
+++ Week-1/SingletonPatternExample/Loggers.cs
@@ -0,0 +1,27 @@
+public class Logger
+{
+    private static Logger _instance;
+    private static readonly object _lock = new object();
+
+    // Private constructor prevents instantiation from outside
+    private Logger() {}
+
+    // Public static method to get the singleton instance
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                    _instance = new Logger();
+            }
+        }
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG] {message}");
+    }
+}
---
+++ Week-1/SingletonPatternExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("This is the first log message.");
+        logger2.Log("This is the second log message.");
+
+        Console.WriteLine($"Are both loggers the same instance? {object.ReferenceEquals(logger1, logger2)}");
+    }
+}
