891fcfb0c7de5d7d9813e47e1cb1fbcc6da1c66c
Jeeeeeeet22 <jeetsahoo2203@gmail.com>
2025-06-22 12:32:23 +0530

    C:\Users\KIIT\6364902-solutions\Week-1\FinancialForecast

---
+++ Week-1/FinancialForecast/Finance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.Write("Enter initial amount: ");
+double initialAmount = double.Parse(Console.ReadLine());
+
+Console.Write("Enter annual growth rate (in %): ");
+double growthRate = double.Parse(Console.ReadLine());
+
+Console.Write("Enter number of years to forecast: ");
+int years = int.Parse(Console.ReadLine());
+
+double futureValue = CalculateFutureValue(initialAmount, growthRate, years);
+Console.WriteLine($"\nFuture Value after {years} years: {futureValue:F2}");
+
+// Recursive function to calculate future value
+double CalculateFutureValue(double amount, double rate, int years)
+{
+    if (years == 0)
+        return amount;
+
+    return CalculateFutureValue(amount * (1 + rate / 100), rate, years - 1);
+}
