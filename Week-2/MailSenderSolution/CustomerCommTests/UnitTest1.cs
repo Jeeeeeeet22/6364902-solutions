2f6eff4a672a7d7b7fcc4dd9fa011a48bd385858
Jeet Sahoo <jeetsahoo2203@gmail.com>
2025-06-29 20:10:32 +0530

    commit

---
+++ Week-2/MailSenderSolution/CustomerCommTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommTests
+{
+    public class Tests
+    {
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailIsSent()
+        {
+            // Arrange
+            var mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var customerComm = new CustomerComm(mockMailSender.Object);
+
+            // Act
+            var result = customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.IsTrue(result);
+            mockMailSender.Verify(x => x.SendMail("cust123@abc.com", "Some Message"), Times.Once);
+        }
+    }
+}
